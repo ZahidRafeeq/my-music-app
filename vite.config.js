6423236f610e1162216e432f9c7d39f1568f6187
Zahid Rafeeq <zahidrafeeque9@gmail.com>
2025-06-12 06:45:46 +0500

    Initial full push with updated code

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/my-music-app/', // <-- IMPORTANT: your repo name
   plugins: [react()],
 });
